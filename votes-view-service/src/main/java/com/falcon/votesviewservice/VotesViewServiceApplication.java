4b29d236cfd29a5d62be5f90c112d6479d08905c
Dharan Aditya <dharan.aditya@outlook.com>
2021-12-30 23:24:29 +0530

    setting up project

---
+++ votes-view-service/src/main/java/com/falcon/votesviewservice/VotesViewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.falcon.votesviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotesViewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotesViewServiceApplication.class, args);
+	}
+
+}
---
+++ votes-view-service/src/test/java/com/falcon/votesviewservice/VotesViewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcon.votesviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotesViewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
